c6b59f5282d7fe6b3e29c72b744d648bd0bde842
Rinigus <rinigus.git@gmail.com>
2017-03-30 23:47:27 +0300

    remove native arch optimization, fixes https://github.com/rinigus/osmscout-server/issues/107

--- download.sh
+++ download.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+set -e
+
 wget http://fallabs.com/kyotocabinet/pkg/kyotocabinet-1.2.76.tar.gz
 tar zxvf kyotocabinet-*.tar.gz
+
+cd kyotocabinet-1.2.76
+patch < ../rm_native_optimization.patch
